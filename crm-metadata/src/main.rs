fc859f483ba8e013e5a1e3f04571e294f4b6838a
JefferyWang <admin@wangjunfeng.com>
2024-07-15 23:07:14 +0800

    feature: support metadata service

---
+++ crm-metadata/src/main.rs
@@ -0,0 +1,23 @@
+use anyhow::Result;
+use crm_metadata::{AppConfig, MetadataService};
+use tonic::transport::Server;
+use tracing::info;
+use tracing_subscriber::{
+    filter::LevelFilter, fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _,
+};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let layer = Layer::new().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+
+    let config = AppConfig::load().expect("Failed to load config");
+
+    let addr = format!("[::1]:{}", config.server.port);
+    let addr = addr.parse().unwrap();
+    let svc = MetadataService::new(config).into_server();
+    info!("GreeterServer listening on {}", addr);
+
+    Server::builder().add_service(svc).serve(addr).await?;
+    Ok(())
+}
